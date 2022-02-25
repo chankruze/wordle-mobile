6bc7461c3c893999e3cbe5b6100a25197881eb9e
chankruze <chankruze@gmail.com>
2022-02-25 18:06:47 +0530

    feat: add title, keyboard and constants
    Signed-off-by: chankruze <chankruze@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView, Platform } from "react-native";
+import { colors } from "./constants";
+import Keyboard from "./components/Keyboard/Keyboard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      {/* title */}
+      <Text style={styles.title}>wordle</Text>
+      {/* map */}
+      
+      {/* keyboard */}
+      <Keyboard />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.black,
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+  title: {
+    color: colors.lightgrey,
+    textTransform: "uppercase",
+    fontSize: 32,
+    fontWeight: "bold",
+    letterSpacing: 7,
   },
 });
